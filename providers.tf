4fa10f475b3bf2e70918e1456d94266f60b12097
Samk2310 <164749346+Samk2310@users.noreply.github.com>
2025-08-28 15:45:19 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "> 4.3.34"
     }
     azuread = {
       source  = "hashicorp/azuread"
