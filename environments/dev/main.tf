246923381761f8ad4a5122213d0b08878dcf3ac0
Samk2310 <164749346+Samk2310@users.noreply.github.com>
2025-08-28 11:42:31 -0400

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,43 +1,20 @@
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
+module "oag_worker" {
+  source       = "../../modules/oag-worker"
+  environment  = var.environment
+  location     = var.location
+  vnet_name    = var.vnet_name
+  subnet_id    = var.subnet_id
+  oag_admin_ip = var.oag_admin_ip
+  oag_token    = var.oag_token
+  worker_count = var.worker_count
+  oag_disk_id  = var.oag_disk_id
 }
 
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network_name
-  address_space       = ["10.1.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "oag_subnet" {
-  name                 = "oag-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.1.1.0/24"]
-}
-
-module "oag_worker_node" {
-  source                = "../../modules/oag_worker"
-  environment           = var.environment
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  subnet_id             = azurerm_subnet.oag_subnet.id
-  hostname              = var.hostname
-  oag_image_uri         = var.oag_image_uri
-  vm_size               = var.vm_size
-}
+variable "environment" {}
+variable "location" {}
+variable "vnet_name" {}
+variable "subnet_id" {}
+variable "oag_admin_ip" {}
+variable "oag_token" { sensitive = true }
+variable "worker_count" {}
+variable "oag_disk_id" {}
