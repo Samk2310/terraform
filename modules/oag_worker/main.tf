28baa94f6066c959d37a1ac7eb63fe65ec161294
Samk2310 <164749346+Samk2310@users.noreply.github.com>
2025-08-28 11:40:36 -0400

    Update main.tf

--- modules/oag_worker/main.tf
+++ modules/oag_worker/main.tf
@@ -1,52 +1,42 @@
-resource "azurerm_network_interface" "oag_worker_nic" {
-  name                = "${var.hostname}-nic"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+resource "azurerm_resource_group" "oag_rg" {
+  name     = "oag-${var.environment}-rg"
+  location = var.location
+}
+
+resource "azurerm_network_interface" "oag_nic" {
+  count               = var.worker_count
+  name                = "oag-worker-${var.environment}-${count.index}-nic"
+  location            = azurerm_resource_group.oag_rg.location
+  resource_group_name = azurerm_resource_group.oag_rg.name
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.oag_worker_ip.id
   }
 }
 
-resource "azurerm_public_ip" "oag_worker_ip" {
-  name                = "${var.hostname}-ip"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_linux_virtual_machine" "oag_worker" {
-  name                  = "${var.hostname}-vm"
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-  size                  = var.vm_size
-  admin_username        = "oag-mgmt"
-  network_interface_ids = [azurerm_network_interface.oag_worker_nic.id]
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = "oag-mgmt"
-    public_key = file("~/.ssh/id_rsa.pub") // Ensure this path is correct
-  }
+resource "azurerm_linux_virtual_machine" "oag_worker_vm" {
+  count               = var.worker_count
+  name                = "oag-worker-${var.environment}-${count.index}-vm"
+  location            = azurerm_resource_group.oag_rg.location
+  resource_group_name = azurerm_resource_group.oag_rg.name
+  size                = "Standard_DS2_v2" # Customize as needed
+  network_interface_ids = [azurerm_network_interface.oag_nic[count.index].id]
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    disk_size_gb         = 64
+    managed_disk_id      = var.oag_disk_id # Re-use the prepared managed disk
   }
 
-  source_image_id = var.oag_image_uri
-}
+  source_image_id = var.oag_disk_id # Re-use the prepared managed disk
 
-output "worker_private_ip" {
-  value = azurerm_network_interface.oag_worker_nic.private_ip_address
-}
+  custom_data = base64encode(templatefile("${path.module}/cloud-init.tpl", {
+    oag_admin_ip = var.oag_admin_ip
+    oag_token    = var.oag_token
+  }))
 
-output "worker_public_ip" {
-  value = azurerm_public_ip.oag_worker_ip.ip_address
+  admin_username = "oktaadmin"
+  disable_password_authentication = true
 }
-
