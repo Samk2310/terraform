d05e84f1963eb454fbeb0799b168e914f86d9ef0
Samk2310 <164749346+Samk2310@users.noreply.github.com>
2025-08-29 17:00:56 -0400

    Update versions.tf

--- environments/dev/versions.tf
+++ environments/dev/versions.tf
@@ -1,10 +1,10 @@
-terraform {
-  required_version = ">= 1.0.0"
-  backend "azurerm" {
-    # Replace with your storage account details for state locking
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "terraformstateoag"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
+#terraform {
+#  required_version = ">= 1.0.0"
+#  backend "azurerm" {
+#    # Replace with your storage account details for state locking
+#    resource_group_name  = "terraform-state-rg"
+#    storage_account_name = "terraformstateoag"
+#    container_name       = "tfstate"
+#    key                  = "terraform.tfstate"
+#  }
+#}
